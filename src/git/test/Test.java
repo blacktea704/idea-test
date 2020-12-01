7ff6867be3643d26aed2bb8dbcab56b30ebf1c08
ZhangYuchao <948804546@qq.com>
2020-12-01 11:37:50 +0800

    第一次测试

---
+++ src/git/test/Test.java
@@ -0,0 +1,17 @@
+package git.test;
+
+/**
+ * @ClassName Test
+ * @Description 一句话描述此类或方法的作用
+ * @Author 张宇超
+ * @Date 2020/12/1 11:21
+ * @Version 1.0
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 2;
+    }
+}
+ 
